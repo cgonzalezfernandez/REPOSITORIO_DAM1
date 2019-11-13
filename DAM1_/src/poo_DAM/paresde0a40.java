332ef4d2058adffdae7053498946ab9c03e916c9
X540 <X540@LAPTOP-IP0IDIL7>
2019-11-13 12:20:09 +0100

    pares

---
+++ DAM1_/src/poo_DAM/paresde0a40.java
@@ -0,0 +1,20 @@
+package poo_DAM;
+
+public class paresde0a40 {
+	
+	public static void main (String[] arg) {
+		
+		System.out.println("LOS PARES DEL 0 AL 40 SON:");
+		
+		for (int i = 0; i <=40; i++) {
+			
+			if((i%2)==0) {
+				
+				System.out.println(i);
+			}
+			
+		}
+		
+	}
+
+}
