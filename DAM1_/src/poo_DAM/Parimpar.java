0c9b4fec62e66696eaf630dc2f9dd4620d2f68ec
Alumno <Alumno@LENOVO-305>
2019-11-13 12:23:06 +0100

    ejercicio sin acabar

---
+++ DAM1_/src/poo_DAM/Parimpar.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package poo_DAM;
+
+/**
+ * @author Alumno
+ *
+ */
+public class Parimpar {
+public static void main(String args[]) {
+	for(int indice=0;indice<=40;indice++) {
+		if(indice%2==0) {
+			System.out.println(indice);
+		}
+		
+	}
+	
+}
+}
