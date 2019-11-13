cee04708414a4a05d58df44f716fe1ecd5e99e91
Alumno <Alumno@LENOVO-301>
2019-11-13 12:26:29 +0100

    programa pares

--- DAM1_/src/poo_DAM/Pares.java
+++ DAM1_/src/poo_DAM/Pares.java
@@ -1,5 +1,23 @@
 package poo_DAM;
 
 public class Pares {
+	public static void main(String[]arg) {
+		
+		for(int indice=0;indice<=40;indice++) {
+			
+			
+			
+			
+			
+		}
+		//Comprobar si es par
+		if(indice%2==0) {
+		System.out.println(indice);	
+		}
+		
+		
+		
+		
+	}
 
 }
