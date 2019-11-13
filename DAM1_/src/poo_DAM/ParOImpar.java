0e88345fa533acd680ea10cb35c99e1343a54e60
Usuario <Usuario@DESKTOP-PNOD0MN>
2019-11-13 12:23:34 +0100

    Pares

---
+++ DAM1_/src/poo_DAM/ParOImpar.java
@@ -0,0 +1,14 @@
+package poo_DAM;
+
+public class ParOImpar {
+
+	public static void main(String[] args) {
+	
+		for(int i=0; i<41; i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+	}
+}
+}
+	
