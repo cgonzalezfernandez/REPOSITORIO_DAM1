8913198b4af6ff8c05a6f0d8a65fa0f6e2c53777
Alumno <Alumno@LENOVO-304>
2019-11-13 12:21:40 +0100

    programa de numeros pares

---
+++ DAM1_/src/poo_DAM/pares.java
@@ -0,0 +1,18 @@
+package poo_DAM;
+import java.util.Arrays;
+public class pares {
+
+
+	public static void main(String[] args) {
+		int array[]=new int[20];
+		for (int i = 2; i <=40; i++) {
+			if (i%2==0) {
+				System.out.println(i);
+				
+			}
+		}
+		
+
+	}
+
+}
