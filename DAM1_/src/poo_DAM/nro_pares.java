5a6871a989960fe63234b6093eff75f630054519
ChriS <ChriS@ChriS>
2019-11-13 12:22:46 +0100

    Programa de números pares.

---
+++ DAM1_/src/poo_DAM/nro_pares.java
@@ -0,0 +1,24 @@
+package poo_DAM;import java.util.Arrays;
+
+public class nro_pares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int nro = 40;
+		
+		int [] numero = new int[nro];
+		
+		
+		for (int i = 2; i <= nro ; i++) {
+			
+			if (i % 2== 0) {
+				
+				
+				System.out.println(i);
+			}
+			
+		}
+	}
+
+}
