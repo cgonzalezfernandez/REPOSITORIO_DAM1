971d3f5bab87940581adffaecbee78e05126c96c
Alumno <Alumno@LENOVO-301>
2019-11-18 08:48:41 +0100

    Gonzalo

--- DAM1_/src/poo_DAM/Pares.java
+++ DAM1_/src/poo_DAM/Pares.java
@@ -2,8 +2,8 @@ package poo_DAM;
 
 public class Pares {
 	public static void main(String[]arg) {
-		
-		for(int indice=0;indice<=40;indice++) {
+		int indice = 0;
+		for(int indice1=0;indice1<=40;indice1++) {
 			
 			
 			
@@ -12,7 +12,7 @@ public class Pares {
 		}
 		//Comprobar si es par
 		if(indice%2==0) {
-		System.out.println(indice);	
+		System.out.println("indice");	
 		}
 		
 		
