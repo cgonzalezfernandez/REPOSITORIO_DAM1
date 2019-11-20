588c77b970b03c08f6d6df1c381c0c6fb3b33ad7
rbilb <rbilb@RBILBAO>
2019-11-20 11:49:18 +0100

    R

--- DAM1_/src/poo_DAM/NumerosPares.java
+++ DAM1_/src/poo_DAM/NumerosPares.java
@@ -5,14 +5,13 @@ public class NumerosPares {
 	public static void main (String [] args) {
 		
 		int num=40;
-	
 		
-		for (int i = 0; i<num; i++) {
+				for (int i = 0; i<num; i++) {
 			
-			if(i%2==0) {
-				System.out.println(i);	
-			}
-		}
+						if(i%2==0) {
+							System.out.println(i);	
+						}
+				}
 			
 	}
 
