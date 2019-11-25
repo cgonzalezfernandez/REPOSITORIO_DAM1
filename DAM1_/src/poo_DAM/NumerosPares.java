9398ea7fc968d3e19732c75afbf8df02dc1974c7
rbilb <rbilb@RBILBAO>
2019-11-25 09:19:22 +0100

    corregido

---
+++ DAM1_/src/poo_DAM/NumerosPares.java
@@ -0,0 +1,19 @@
+package poo_DAM;
+
+
+public class NumerosPares {
+	public static void main (String [] args) {
+		
+		int num=40;
+		
+			for (int i = 0; i<num; i++) {
+			
+						if(i%2==0) {
+							System.out.println(i);	
+						}
+			}
+			
+	}
+
+}
+
