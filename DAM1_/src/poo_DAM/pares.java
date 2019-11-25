d78f23e5ce7a822a30ecdc7699734a67bc95bd31
Usuario <Usuario@DESKTOP-PNOD0MN>
2019-11-25 09:18:20 +0100

    ferpecto

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -1,16 +1,19 @@
 package poo_DAM;
 
+import java.util.Arrays;
+
 public class pares {
 	public static void main(String[]arg) {
-		int array[]=new int[20];
+		int array[]=new int[10];
+		int j=0;
 		for(int i=0;i<=10;i++) {
 			if(i%2==0) {
 				System.out.println(i+" es par");
-				for(int j=0;j<array.length;j++) {
 					array[j]=i;
-				}
-			} 
+					j++;
+			}
 		}
+		System.out.println(Arrays.toString(array));
 		System.out.println("Se acabo el programa");
 	}
 }
