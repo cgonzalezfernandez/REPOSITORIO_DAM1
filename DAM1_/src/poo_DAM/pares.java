1f12f0a833227ce81d26697a2ca9f508c98f4251
oscar <oscar@LAPTOP-9U97QEV4>
2019-11-25 08:41:51 +0100

    oscar_corregido

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -1,15 +1,18 @@
 package poo_DAM;
 
+import java.util.Arrays;
+
 public class pares {
 	public static void main(String[]arg) {
 		int array[]=new int[20];
-		for(int i=0;i<=40;i++) {
+		int cont=0;
+		for(int i=0;i<=10;i++) {
 			if(i%2==0) {
 				System.out.println(i+" es par");
-				for(int j=0;j<array.length;j++) {
-					array[j]=i;
-				}
+					array[cont]=i;			
+					cont++;
 			} 
 		}
+		System.out.println(Arrays.toString(array));
 	}
 }
