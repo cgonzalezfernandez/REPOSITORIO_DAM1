a557574dd1cb36b3e812e39acb2240a78b876847
loiga <loiga@DESKTOP-85197II>
2019-11-25 08:40:14 +0100

    //hola de nuevo

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -3,7 +3,7 @@ package poo_DAM;
 public class pares {
 	public static void main(String[]arg) {
 		int array[]=new int[20];
-		for(int i=0;i<=40;i++) {
+		for(int i=0;i<=10;i++) {
 			if(i%2==0) {
 				System.out.println(i+" es par");
 				for(int j=0;j<array.length;j++) {
@@ -11,5 +11,6 @@ public class pares {
 				}
 			} 
 		}
+		System.out.println("Fin");
 	}
 }
