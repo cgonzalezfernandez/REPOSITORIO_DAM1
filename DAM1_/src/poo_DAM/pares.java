3b14c9c84228793d9c31dd5131ebafff3ea6d948
PabloG <Pablo@DESKTOP-EHMPQCK>
2019-11-25 09:21:13 +0100

    la wea

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -3,13 +3,19 @@ package poo_DAM;
 public class pares {
 	public static void main(String[]arg) {
 		int array[]=new int[20];
-		for(int i=0;i<=40;i++) {
+		
+		for(int i=0;i<=10;i++) {
+			int contador= 0;
+			int almacenar = 0;
+			contador = i;
 			if(i%2==0) {
+				contador++;
+				almacenar = contador;
 				System.out.println(i+" es par");
-				for(int j=0;j<array.length;j++) {
-					array[j]=i;
+				
+					
 				}
 			} 
 		}
+		
 	}
-}
