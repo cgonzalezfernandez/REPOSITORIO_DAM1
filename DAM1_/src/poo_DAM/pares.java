913ceacc2d6c7eb3a20dae891931001023656e42
rbilb <rbilb@RBILBAO>
2019-11-25 09:17:45 +0100

    mensaje

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -2,14 +2,15 @@ package poo_DAM;
 
 public class pares {
 	public static void main(String[]arg) {
-		int array[]=new int[21];
-		int par=0;
-			for(int i=0;i<=40;i++) {
+		int array[]=new int[6];
+		int x=0;
+			for(int i=0;i<=10;i++) {
 				if(i%2==0){
-						par=i;
-						array[i]+=par;
-						System.out.print(array);	
+						System.out.println(i + " es par.");
+						array[x]=i;			
+						x++;
 				}	
 			}
+			System.out.println("Se acabó el programa");
 	}
 }
