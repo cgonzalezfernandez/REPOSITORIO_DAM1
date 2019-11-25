e634b357ac53bec504dd29c0c97b95f8c9a86283
Moroni <Moroni@LAPTOP-I16PJ8AO>
2019-11-25 09:18:13 +0100

    Array corregido

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -2,13 +2,12 @@ package poo_DAM;
 
 public class pares {
 	public static void main(String[]arg) {
-		int array[]=new int[5];
+		int array[]=new int[6],j=0;
 		for(int i=0;i<=10;i++) {
 			if(i%2==0) {
 				System.out.println(i+" es par");
-				for(int j=0;j<array.length;j++) {
 					array[j]=i;
-				}
+					j++;
 			} 
 		}
 		System.out.println("Se acabo el programa");
