249318ae84881703f11fa2b2abc244b6bdc828bf
Moroni <Moroni@LAPTOP-I16PJ8AO>
2019-11-25 08:47:58 +0100

    Agregado array

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -2,8 +2,8 @@ package poo_DAM;
 
 public class pares {
 	public static void main(String[]arg) {
-		int array[]=new int[20];
-		for(int i=0;i<=40;i++) {
+		int array[]=new int[5];
+		for(int i=0;i<=10;i++) {
 			if(i%2==0) {
 				System.out.println(i+" es par");
 				for(int j=0;j<array.length;j++) {
