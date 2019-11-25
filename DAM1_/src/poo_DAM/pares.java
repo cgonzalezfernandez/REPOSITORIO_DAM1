8d4a8b3bdb350d96b000e25737eb5b632b813a83
Alumno <Alumno@LENOVO-301>
2019-11-25 09:21:30 +0100

    Gonzalo Gomez hoyal

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -3,13 +3,17 @@ package poo_DAM;
 public class pares {
 	public static void main(String[]arg) {
 		int array[]=new int[20];
-		for(int i=0;i<=40;i++) {
+		int cont=0;
+		for(int i=0;i<=10;i++) {
 			if(i%2==0) {
 				System.out.println(i+" es par");
-				for(int j=0;j<array.length;j++) {
-					array[j]=i;
-				}
+				
+					array[cont]=i;
+					cont++;
+				
 			} 
 		}
+		System.out.println("Se acabo el programa");
 	}
+	
 }
