f36ab75af0237e7216443103fb5ebea7f843fb0e
X540 <X540@LAPTOP-IP0IDIL7>
2019-11-25 09:20:41 +0100

    Marta Barcena

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -2,14 +2,20 @@ package poo_DAM;
 
 public class pares {
 	public static void main(String[]arg) {
-		int array[]=new int[20];
-		for(int i=0;i<=40;i++) {
+		int array[]=new int[10];
+		int j=0;
+		for(int i=0;i<=10;i++) {
 			if(i%2==0) {
 				System.out.println(i+" es par");
-				for(int j=0;j<array.length;j++) {
+				
+					
 					array[j]=i;
-				}
+					j++;
+				
 			} 
 		}
+		
+		System.out.println("se acabó el programa");
 	}
+	
 }
