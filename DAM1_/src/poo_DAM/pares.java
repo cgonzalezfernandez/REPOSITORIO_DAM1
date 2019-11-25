15893e1ed070812fd416ee34f01dd14c17e96857
Manuel Martin <man.ual@hotmail.com>
2019-11-25 09:18:00 +0100

    Fixed for loop

--- DAM1_/src/poo_DAM/pares.java
+++ DAM1_/src/poo_DAM/pares.java
@@ -3,13 +3,14 @@ package poo_DAM;
 public class pares {
 	public static void main(String[]arg) {
 		int array[]=new int[20];
+		int indexI = 0;
 		for(int i=0;i<=40;i++) {
 			if(i%2==0) {
 				System.out.println(i+" es par");
-				for(int j=0;j<array.length;j++) {
-					array[j]=i;
-				}
+				indexI = i;
+					array[indexI]=i;
 			} 
 		}
+		System.out.println("se acabo");
 	}
 }
